81bd5adc044bde71063d73ef34e3c7edd6f7839b
taniytanya <taniytanya@gmail.com>
2022-09-11 20:51:29 +0300

    add remote

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -11,5 +11,6 @@ public class TestBase {
     static void configure() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
