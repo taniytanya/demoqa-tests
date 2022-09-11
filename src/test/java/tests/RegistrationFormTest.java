7ce5d07cf5bdd2e12507183e729e6795b011c70e
taniytanya <taniytanya@gmail.com>
2022-09-11 20:16:50 +0300

    add allure

--- src/test/java/tests/RegistrationFormTest.java
+++ src/test/java/tests/RegistrationFormTest.java
@@ -1,10 +1,15 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import com.github.javafaker.Faker;
 import utils.RandomUtils;
 
+import static io.qameta.allure.Allure.step;
 import static java.lang.String.format;
 import static tests.TestData.*;
 
@@ -38,57 +43,84 @@ public class RegistrationFormTest extends TestBase {
         userAddress = faker.address().fullAddress();
         userGender = RandomUtils.getRandomGender();
         userHobby = RandomUtils.getRandomHobby();
-        userFullName = format("%s %s",firstName, lastName);
-        userDateOfBirth = format("%s %s,%s",day,month,year);
+        userFullName = format("%s %s", firstName, lastName);
+        userDateOfBirth = format("%s %s,%s", day, month, year);
 
     }
 
     @Test
+    @DisplayName("Success Registration on demoqa with All Fields")
+    @Owner("Tanya")
+    @Story("Demoqa registration form")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "Testing", url = "https://demoqa.com")
     void successRegistrationAllFields() {
-        registrationFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setPhoneNumber(userPhoneNumber)
-                .setEmail(userEmail)
-                .setGender(userGender)
-                .setBirthDay(day, month, year)
-                .setSubject(userSubject)
-                .setHobby(userHobby)
-                .uploadPicture(userPicture)
-                .setAddress(userAddress)
-                .setStateAndCity(userState, userCity)
-                .submit();
-        String userStateAndCity = format("%s %s", userState, userCity);
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
-        registrationFormPage.checkResultTableVisible();
-        registrationFormPage.checkResult("Student Name", userFullName)
-                .checkResult("Student Email", userEmail)
-                .checkResult("Gender", userGender)
-                .checkResult("Mobile", userPhoneNumber)
-                .checkResult("Date of Birth", userDateOfBirth)
-                .checkResult("Subjects", userSubject)
-                .checkResult("Hobbies", userHobby)
-                .checkResult("Picture", shortPicture)
-                .checkResult("Address", userAddress)
-                .checkResult("State and City",userStateAndCity);
+        step("Open main page and insert data in all fields", () -> {
+            registrationFormPage.openPage()
+                    .setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setPhoneNumber(userPhoneNumber)
+                    .setEmail(userEmail)
+                    .setGender(userGender)
+                    .setBirthDay(day, month, year)
+                    .setSubject(userSubject)
+                    .setHobby(userHobby)
+                    .uploadPicture(userPicture)
+                    .setAddress(userAddress)
+                    .setStateAndCity(userState, userCity)
+                    .submit();
+            ;
+        });
+
+        step("Check result modal window is open", () -> {
+            registrationFormPage.checkResultTableVisible();
+        });
+        step("Check result data on registration form", () -> {
+            String userStateAndCity = format("%s %s", userState, userCity);
+            registrationFormPage.checkResult("Student Name", userFullName)
+                    .checkResult("Student Email", userEmail)
+                    .checkResult("Gender", userGender)
+                    .checkResult("Mobile", userPhoneNumber)
+                    .checkResult("Date of Birth", userDateOfBirth)
+                    .checkResult("Subjects", userSubject)
+                    .checkResult("Hobbies", userHobby)
+                    .checkResult("Picture", shortPicture)
+                    .checkResult("Address", userAddress)
+                    .checkResult("State and City", userStateAndCity);
+        });
 
 
     }
 
     @Test
+    @DisplayName("Success Registration on demoqa with only Required Fields")
+    @Owner("Tanya")
+    @Story("Demoqa registration form")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "Testing", url = "https://demoqa.com")
     void successRegistrationRequiredFields() {
-        registrationFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setPhoneNumber(userPhoneNumber)
-                .setGender(userGender)
-                .setBirthDay(day, month, year)
-                .submit();
-        registrationFormPage.checkResultTableVisible();
-        registrationFormPage.checkResult("Student Name", userFullName)
-                .checkResult("Gender", userGender)
-                .checkResult("Mobile", userPhoneNumber)
-                .checkResult("Date of Birth", userDateOfBirth);
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Open main page and insert data in only Required fields", () -> {
+            registrationFormPage.openPage()
+                    .setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setPhoneNumber(userPhoneNumber)
+                    .setGender(userGender)
+                    .setBirthDay(day, month, year)
+                    .submit();
+        });
+        step("Check result modal window is open", () -> {
+            registrationFormPage.checkResultTableVisible();
+        });
+        step("Check result data on registration form", () -> {
+            registrationFormPage.checkResult("Student Name", userFullName)
+                    .checkResult("Gender", userGender)
+                    .checkResult("Mobile", userPhoneNumber)
+                    .checkResult("Date of Birth", userDateOfBirth);
+        });
 
     }
 }
